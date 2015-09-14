d5d784164586901a48e0a57eca5b7e597b4afcee
0x79H <lingxuyangf1@hotmail.com>
2015-09-15 00:58:45 +0800

    -

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 #include <getopt.h>
 
 int main(int argc,char *argv[]){
+    FILE *fp;
+    filename = 'D:\empty_out.log'
+    //if argv[1]=out -->filename=argv[2]
+	made = a+;
+    if(fp = fopen(char* filename, char* made)) exit(0);
+    for (int i=1,i<=argc,i++){
+        fprintf(fp,"%s",argv[i]);
+        }
+    fclose(fp);
     return 0;
     }
-    
-    
